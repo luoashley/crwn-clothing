985257a1d52c8d7e803ea1a527bab1bf116b1862
luoashley <97135228+luoashley@users.noreply.github.com>
2022-09-04 07:51:54 -0400

    finished up the sign in, sign up, authentication component, and some styling in the authentication page.

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword} from 'firebase/auth';
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
 import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
@@ -53,5 +53,9 @@ export const createUserDocumentFromAuth = async (
 export const createAuthUserWithEmailAndPassword = async (email,password) => {
     if(!email || !password) return;
     return await createUserWithEmailAndPassword(auth, email, password);
+};
 
+export const signInAuthUserWithEmailAndPassword = async (email,password) => {
+    if(!email || !password) return;
+    return await signInWithEmailAndPassword(auth, email, password);
 };
