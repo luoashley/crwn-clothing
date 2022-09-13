b3dc1cc45ebfd56b31781f19be94e9c0d7d31482
luoashley <97135228+luoashley@users.noreply.github.com>
2022-09-13 15:09:41 -0400

    adding cart context and shopping icon as well as cart drop-down.

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,26 +1,14 @@
 import {createContext, useState, useEffect} from 'react';
-import {createUserDocumentFromAuth, onAuthStateChangedListener} from "../utils/firebase/firebase.utils";
+import PRODUCTS from '../shop-data.json';
 
-export const UserContext = createContext({
-    currentUser: null,
-    setCurrentUser: () => null,
+export const ProductsContext = createContext({
+    products: [],
+
 });
 
-export const UserProvider = ({children}) => {
-    const [currentUser, setCurrentUser]= useState(null);
-    const value = {currentUser, setCurrentUser};
+export const ProductsProvider = ({children}) => {
+    const [products, setProducts] = useState(PRODUCTS);
+    const value = {products}
 
-    useEffect(()=> {
-        const unsubscribe = onAuthStateChangedListener((user) => {
-            if(user){
-                createUserDocumentFromAuth(user);
-            }
-            setCurrentUser(user);
-        });
-
-        return unsubscribe;
-    },[]);
-
-
-    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+    return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
 };
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,8 @@ import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
 import './navigation.styles.scss'
 import {UserContext} from "../../contexts/user.context";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
     const {currentUser} = useContext(UserContext);
@@ -23,8 +25,9 @@ const Navigation = () => {
                         ) : (
                             <Link className='nav-link' to='/auth'>SIGN IN</Link>
                     )}
-
+                    <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet />
         </Fragment>
