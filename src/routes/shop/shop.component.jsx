3d4b900f4ff46a3896508bc1457d12c87ebb9eae
luoashley <97135228+luoashley@users.noreply.github.com>
2022-12-27 01:15:41 -0500

    Update Firebase Data Storage

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,16 +1,14 @@
-import {useContext} from "react";
-import {ProductsContext} from "../../contexts/products.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import {Route, Routes} from "react-router-dom";
 import './shop.styles.scss';
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 const Shop = () => {
-    const {products}  = useContext(ProductsContext);
     return(
-        <div className='products-container'>
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product}/>
-                ))}
-        </div>
+        <Routes>
+            <Route index element={<CategoriesPreview />} />
+            <Route path=":category" elment={<Category />}  />
+        </Routes>
     )
 };
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -8,7 +8,7 @@ import {getAuth,
     signOut,
     onAuthStateChanged
 } from 'firebase/auth';
-import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
+import {getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -32,6 +32,29 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
 export const signInWithGoogleRedirect = () => signInWithGoogleRedirect(auth, googleProvider);
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd, field='title') => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+    objectsToAdd.forEach((object)=>{
+        const docRef = doc(collectionRef, object[field].toLowerCase());
+        batch.set(docRef, object);
+     });
+    await batch.commit();
+    console.log('done');
+};
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const {title, items} = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    },{});
+    return categoryMap;
+}
 
 export const createUserDocumentFromAuth = async (
     userAuth,
